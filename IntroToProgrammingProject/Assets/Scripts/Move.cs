e2d4516a3cb8da3af769c39790471e2a2655c75d
ZigZogger <ziggycooperhenry@gmail.com>
2022-04-09 14:59:08 -0700

    4/9/22

--- IntroToProgrammingProject/Assets/Scripts/Move.cs
+++ IntroToProgrammingProject/Assets/Scripts/Move.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
     Rigidbody rb;
+
+    private Vector3 movementDir;
+    private Vector3 newPosition;
+    private Vector2 lastMousePosition;
+    public float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +21,49 @@ public class Move : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
-
+        Vector3 moveRight = transform.right * movementDir.x;
+        Vector3 moveForward = transform.forward * movementDir.z;
+        Vector3 newPosition = transform.position + ((moveRight + moveForward) * speed * Time.deltaTime);
         rb.MovePosition(newPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 rotationAmount = mousePosition - lastMousePosition;
 
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 force = new Vector3(0.0f, jumpForce, 0.0f);
+            rb.AddForce(force, ForceMode.Impulse);
+        }
+
+        Vector3 capsuleRotate = new Vector3(0.0f, rotationAmount.x * rotationSpeed * Time.deltaTime, 0.0f);
+        transform.Rotate(capsuleRotate);
+
+        movementDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementDir.z = 1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementDir.x = -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementDir.z = -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementDir.x = 1.0f;
+        }
+
+        lastMousePosition = mousePosition;
     }
 }
